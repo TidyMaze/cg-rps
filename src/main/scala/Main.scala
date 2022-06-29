d16fda3022dee9db0c7823fbab52def6536fe8f0
Yann Rolland <yann.rolland@iadvize.com>
2022-06-29 21:18:02 +0200

    init

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,19 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+import math._
+import scala.util._
+import scala.io.StdIn._
+
+/** Auto-generated code below aims at helping you parse
+  * the standard input according to the problem statement.
+  */
+object Player extends App {
+
+  // game loop
+  while (true) {
+    val previousOpponentMove = readLine
+
+    // Write an action using println
+    // To debug: Console.err.println("Debug messages...")
+
+    println("ROCK")
   }
 }
