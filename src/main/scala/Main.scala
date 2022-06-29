d6dd894c6d288dbcf1fdbb22d4f957187999eef2
Yann Rolland <yann.rolland@iadvize.com>
2022-06-29 21:56:57 +0200

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,19 +3,26 @@ import scala.util._
 import scala.io.StdIn._
 
 object Player extends App {
+  val allStrategies: Seq[Strategy] = Seq(new RandomStrategy)
 
-  val random = new Random()
+  def parse(readLine: String): Moves.Value =
+    readLine match {
+      case "ROCK"     => Moves.ROCK
+      case "PAPER"    => Moves.PAPER
+      case "SCISSORS" => Moves.SCISSORS
+      case _          => throw new IllegalArgumentException("Invalid move")
+    }
 
-  def randomIn(values: Moves.ValueSet) = {
-    val index = random.nextInt(values.size)
-    values.toList(index)
-  }
+  var strategiesScores = allStrategies.map(_ -> 0.toDouble).toMap
 
   while (true) {
-    val previousOpponentMove = readLine
+    val previousOpponentMove = parse(readLine)
 
-    // get random move
-    val move = randomIn(Moves.values)
+    strategiesScores = strategiesScores.map { case (strategy, score) =>
+      (strategy, score + strategy.getScore(previousOpponentMove))
+    }
+
+    val move = strategiesScores.maxBy(_._2)._1.move(previousOpponentMove)
 
     println(move)
   }
@@ -24,4 +31,27 @@ object Player extends App {
 object Moves extends Enumeration {
   type Move = Value
   val ROCK, PAPER, SCISSORS = Value
+}
+
+trait Strategy {
+  def getScore(previousOpponentMove: Moves.Value): Double
+
+  def move(previousOpponentMove: Moves.Value): Moves.Value
+}
+
+class RandomStrategy extends Strategy {
+
+  val random = new Random()
+
+  override def move(previousOpponentMove: Moves.Value): Moves.Value = {
+    randomIn(Moves.values)
+
+  }
+
+  def randomIn(values: Moves.ValueSet) = {
+    val index = random.nextInt(values.size)
+    values.toList(index)
+  }
+
+  override def getScore(previousOpponentMove: Moves.Value): Double = 1.0 / 3.0
 }
